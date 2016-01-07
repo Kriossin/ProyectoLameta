8647a60b3eeffe5ff4b22e8acfa86b2cf53d46cf
Lucia <lferrerfct@gmail.com>
2016-01-07 11:14:51 +0100

    .

--- AgendaLameta/app/src/main/java/com/example/lameta/agendalameta/detail/EventoDetail.java
+++ AgendaLameta/app/src/main/java/com/example/lameta/agendalameta/detail/EventoDetail.java
@@ -1,7 +1,13 @@
 package com.example.lameta.agendalameta.detail;
 
+import android.widget.Button;
+import android.widget.EditText;
+
 /**
  * Created by Bisquert on 11/12/2015.
  */
 public class EventoDetail {
+    Button btnModificar, btnBorrar;
+    EditText nombreEvento, fecha, lugar, hora, etiqueta;
+
 }
