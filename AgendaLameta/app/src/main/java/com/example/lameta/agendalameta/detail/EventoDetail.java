e6c0ba8e9282bcf40800eddb4fc3a0de192994e1
Lucia <lferrerfct@gmail.com>
2016-01-08 16:53:44 +0100

    EventoDetail,activity_evento y eliminado activity_anyadirevento

--- AgendaLameta/app/src/main/java/com/example/lameta/agendalameta/detail/EventoDetail.java
+++ AgendaLameta/app/src/main/java/com/example/lameta/agendalameta/detail/EventoDetail.java
@@ -1,5 +1,7 @@
 package com.example.lameta.agendalameta.detail;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -8,6 +10,10 @@ import android.widget.EditText;
 
 import com.example.lameta.agendalameta.DAO.EventoDAO;
 import com.example.lameta.agendalameta.R;
+import com.example.lameta.agendalameta.model.Evento;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 //import com.example.lameta.agendalameta.R;
 
@@ -16,29 +22,39 @@ import com.example.lameta.agendalameta.R;
  */
 public class EventoDetail extends ActionBarActivity {
     Button btnModificar, btnBorrar;
+    private int Evento_Id=0;
 
     @Override
-protected void onCreate(Bundle savedInstanceState){
-super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_evento);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evento);
 
         btnModificar = (Button) findViewById(R.id.modificar);
         btnBorrar = (Button) findViewById(R.id.borrar);
 
-      // btnModificar.setOnClickListener(this);
-      // btnBorrar.setOnClickListener(this);
+      //  btnModificar.setOnClickListener(this);
+      //  btnBorrar.setOnClickListener(this);
+
+        Evento_Id = 0;
+        Intent intent = getIntent();
+        Evento_Id=intent.getIntExtra("Evento_Id",0);
+       // EventoDAO eventoDAO = new EventoDAO(this);
+        Evento evento = new Evento();
+      //  evento = eventoDAO.getEventoById(Evento_Id);
 
 
 
 
-}
 
-    public void onClick(View v){
-        if(v == findViewById(R.id.modificar)){
-            //EventoDAO eventoDAO = new EventoDAO(this);
-        }
     }
 
+    public void onClick(View v) {
+        if (v == findViewById(R.id.modificar)) {
+          //  EventoDAO eventoDAO = new EventoDAO(this);
+            Evento evento = new Evento();
+
+        }
+    }
 
 
 }
